cba1d5704d2568c832970a4d74c92a41f0e4a795
ItsJollybae <itsjollybae@icloud.com>
2025-02-05 18:27:16 +0300

    first commit

---
+++ form_ajax.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    $("form").submit(function (event) {
+      var formData = {
+        user_name: $("#user_name").val(),
+        user_last_name: $("#user_last_name").val(),
+        user_second_name: $("user_second_name").val()
+      };
+  
+      $.ajax({
+        type: "POST",
+        url: "dadata.php",
+        data: formData,
+        dataType: "json",
+        encode: true,
+      })
+      .done(function (result) {
+        console.log('Имя:', result.user_name);
+        console.log('Отчество:', result.user_second_name);
+        console.log('Фамилия:', result.user_last_name); 
+      for (let value of result) {
+  
+          $("#result").html(
+              '<p>Полное имя ' + value.result + '</p>' +
+              '<p>В лице ' + value.result_genitive + '</p>' +
+              '<p>Кому ' + value.result_dative + '</p>' +
+              '<p>Согласовано ' + value.result_ablative + '</p>'
+            );
+      }
+  
+      });
+  
+      event.preventDefault();
+    });
+  });
